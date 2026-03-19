4f4d9f130761d9a44ca7e1ca62d1fb5fc0374dd9
Andriy <81444060+DeepDF@users.noreply.github.com>
2026-03-19 20:13:38 +0200

    lab1

---
+++ lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/// <summary>
+/// Головний клас програми.
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class HelloWorld
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
